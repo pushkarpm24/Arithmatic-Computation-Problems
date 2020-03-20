9c054e1b713799ba7283ffe6cee10a3c27a06839
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-20 10:26:09 +0530

    ADDED: code to computer fourth operation

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-#Problem Statement ->  Compute c+a/b
+#Problem Statement ->  Compute a%b+c
 #Author -> Pushkar Morey
 #Date -> 20/03/2020
 
@@ -9,16 +9,17 @@ read -p "Enter second number to perform operation :" numberTwo
 read -p "Enter third number to perform operation :" numberThree
 
 #FIRST OPERATION AS GIVEN
-
 resultOne=$(( $numberOne + $numberTwo * $numberThree ))
 echo "First result= $resultOne"
 
 #SECOND OPERATION AS GIVEN
-
 resultTwo=$(( $numberOne * $numberTwo + $numberThree ))
 echo "Second result= $resultTwo"
 
 #THIRD OPERATION AS GIVEN
-
 resultThree=$(( $numberThree + $numberOne / $numberTwo ))
 echo "Third result= $resultThree"
+
+#FOURTH OPERATION AS GIVEN
+resultFour=$(( $numberOne % $numberTwo + $numberThree ))
+echo "Fourth result= $resultThree"
