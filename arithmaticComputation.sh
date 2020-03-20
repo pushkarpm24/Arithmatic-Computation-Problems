bc602927c576cf27916369ac11dc2ce6a2deda15
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-20 10:51:31 +0530

    ADDED: code to store the results in dictionary

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -4,22 +4,28 @@
 #Author -> Pushkar Morey
 #Date -> 20/03/2020
 
+declare -A resultStore
+resultStore=([result1]=0 [result2]=0 [result3]=0 [result4]=0)
+
 read -p "Enter first number to perform opertion :" numberOne
 read -p "Enter second number to perform operation :" numberTwo
 read -p "Enter third number to perform operation :" numberThree
 
 #FIRST OPERATION AS GIVEN
 resultOne=$(( $numberOne + $numberTwo * $numberThree ))
-echo "First result= $resultOne"
+resultStore[result1]=$resultOne
 
 #SECOND OPERATION AS GIVEN
 resultTwo=$(( $numberOne * $numberTwo + $numberThree ))
-echo "Second result= $resultTwo"
+resultStore[result2]=$resultTwo
 
 #THIRD OPERATION AS GIVEN
 resultThree=$(( $numberThree + $numberOne / $numberTwo ))
-echo "Third result= $resultThree"
+resultStore[result3]=$resultThree
 
 #FOURTH OPERATION AS GIVEN
 resultFour=$(( $numberOne % $numberTwo + $numberThree ))
-echo "Fourth result= $resultThree"
+resultStore[result4]=$resultFour
+
+echo ${!resultStore[@]}
+echo ${resultStore[@]}
