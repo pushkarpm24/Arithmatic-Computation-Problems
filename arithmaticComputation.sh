2e4811ed9665d0c9d5138a8eb60ff257d7cb50c1
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-20 09:26:36 +0530

    ADDED: code to perform first operation

--- arithmaticComputation.sh
+++ arithmaticComputation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-#Problem Statement ->  Write aprogram to take three inputs a,b&c
+#Problem Statement ->  Compute a+b*c
 #Author -> Pushkar Morey
 #Date -> 20/03/2020
 
@@ -8,3 +8,7 @@ read -p "Enter first number to perform opertion :" numberOne
 read -p "Enter second number to perform operation :" numberTwo
 read -p "Enter third number to perform operation :" numberThree
 
+#FIRST OPERATION AS GIVEN
+
+resultOne=$(( $numberOne + $numberTwo * $numberThree ))
+echo "First result= $resultOne"
