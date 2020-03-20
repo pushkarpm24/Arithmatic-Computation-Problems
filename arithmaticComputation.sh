9a6653d990551757ff26983da3df5624e1a32253
pushkarpm24 <pushkarmorey555@gmail.com>
2020-03-20 08:58:25 +0530

    ADDED: code to take three input number from user

---
+++ arithmaticComputation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+#Problem Statement ->  Write aprogram to take three inputs a,b&c
+#Author -> Pushkar Morey
+#Date -> 20/03/2020
+
+read -p "Enter first number to perform opertion :" numberOne
+read -p "Enter second number to perform operation :" numberTwo
+read -p "Enter third number to perform operation :" numberThree
+
